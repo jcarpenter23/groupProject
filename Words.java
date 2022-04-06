2db31a80ab35c9ab89ae0ba512a28e23966d38a4
mylemeter <gjones4@mail.umw.edu>
2022-04-06 11:27:43 -0400

    classes created

---
+++ Words.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Words {
+    ArrayList nouns=new ArrayList<String>();
+    ArrayList adjectives=new ArrayList<String>();
+    ArrayList verbs=new ArrayList<String>();
+}
