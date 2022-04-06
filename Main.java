2325ba8036427aa3b491956a1295ec81defdd31d
jvann <redmysticjv01@gmail.com>
2022-04-06 11:36:10 -0400

    Added the search method in Main

--- Main.java
+++ Main.java
@@ -1,2 +1,32 @@
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static ArrayList<String> getStory() {
+        ArrayList<String> storyList = new ArrayList<>();
+
+        File f = new File("story");
+
+        FilenameFilter filter = new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String name) {
+                return name.endsWith("story");
+            }
+        };
+        File storyFiles[] = f.listFiles(filter);
+
+
+        if ( storyFiles == null ) {
+            System.out.println("THERE ARE NO FILES");
+        } else
+            for (int o = 0; o < storyFiles.length; o++) {
+                //System.out.println(o+ "\"" + deckFiles[o].getName());
+                storyList.add(storyFiles[o].getName());
+            }
+
+
+        return storyList;
+    }
 }
