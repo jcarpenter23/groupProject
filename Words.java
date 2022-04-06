dad98754df4af96610a907bc8ac0c43d55006b57
mylemeter <gjones4@mail.umw.edu>
2022-04-06 11:31:21 -0400

    getmethods in words created

--- Words.java
+++ Words.java
@@ -4,4 +4,14 @@ public class Words {
     ArrayList nouns=new ArrayList<String>();
     ArrayList adjectives=new ArrayList<String>();
     ArrayList verbs=new ArrayList<String>();
+
+    public ArrayList getNouns(){
+        return nouns;
+    }
+    public ArrayList getAdjectives(){
+        return adjectives;
+    }
+    public ArrayList getVerbs(){
+        return verbs;
+    }
 }
