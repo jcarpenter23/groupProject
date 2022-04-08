5fb3c11a63c0bc7c53167782f7d50ac1e298887b
mylemeter <gjones4@mail.umw.edu>
2022-04-08 10:05:15 -0400

    Story constructor created

--- Story.java
+++ Story.java
@@ -5,6 +5,10 @@ import java.io.PrintWriter;
 public class Story {
     private String fileName;
 
+    public Story(String fileName) {
+        this.fileName = fileName;
+    }
+
     public void replace(String input, String initial){
 
     }
