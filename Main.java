f71ead6bbc527bbd037e1cad88233d496776b5dd
mylemeter <gjones4@mail.umw.edu>
2022-04-20 10:47:09 -0400

    main spacing

--- Main.java
+++ Main.java
@@ -1,23 +1,9 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
-import java.awt.Color;
-
-
 public class Main {
     public static void main(String[] args) {
         //This is to call in the Gui commands, Which will process all the methods that involve pulling the Stories from
         //the Directory and printing them out.
         Gui.gui();
     }
-
 }
 
 
