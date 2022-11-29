ac326c03bebc58c12c72d705b223d92316c4f381
TomatoFriesLAN <94922062+TomatoFriesLAN@users.noreply.github.com>
2022-11-29 10:38:48 -0500

    added question set file reading (not tested)

--- src/QuestionSet.java
+++ src/QuestionSet.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -5,8 +9,63 @@ public class QuestionSet {
     private final ArrayList<Question> questions;
     private static final Random random = new Random();
     
+    private final String filename;
+    
+    private final ArrayList<String> categories;
+    
     public QuestionSet(ArrayList<Question> questions) {
         this.questions = questions;
+        filename = "";
+        categories = new ArrayList<>();
+    }
+    
+    public QuestionSet(String filename) throws IOException {
+        questions = new ArrayList<>();
+        categories = new ArrayList<>();
+        this.filename = filename;
+    
+        FileReader fr = new FileReader(filename);
+        BufferedReader br = new BufferedReader(fr);
+        
+        String currentCategory;
+        String currentQuestion;
+        ArrayList<String> answers;
+        
+        while (true) {
+            try {
+                currentCategory = br.readLine();
+            } catch (IOException e) {
+                break;
+            }
+            
+            if (currentCategory == null) {
+                break; // end of file
+            }
+            
+            while (true) {
+                try {
+                    currentQuestion = br.readLine();
+                    
+                    if (currentQuestion == null || currentQuestion.isEmpty()) {
+                        break; // blank line, so next line starts a new category, or end of file
+                    }
+                    
+                    answers = new ArrayList<>();
+                    for (int i = 0; i < 4; i++) {
+                        answers.add(br.readLine());
+                    }
+                    
+                } catch (IOException e) {
+                    break;
+                }
+                
+                if (answers.contains(null)) {
+                    break; // end of file
+                }
+                
+                questions.add(new Question(currentQuestion, answers, currentCategory));
+            }
+        }
     }
     
     public ArrayList<Question> getQuestions() {
