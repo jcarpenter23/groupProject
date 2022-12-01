0077c43ab249d1ee7f3e67b5c2376fe0bcf31bcc
TomatoFriesLAN <94922062+TomatoFriesLAN@users.noreply.github.com>
2022-11-30 21:00:32 -0500

    expanded functionality of game logic class

--- src/Game.java
+++ src/Game.java
@@ -8,6 +8,14 @@ public class Game {
     private Question currentQuestion;
     private final int crownsToWin;
     
+    private ArrayList<Player> crownHolders;
+    
+    private ArrayList<Long> crownTimes;
+    
+    private Player winner;
+    
+    private static final Player NO_ONE = new Player("No one", 0, 0);
+    
     public Game(int playerCount, File setFile, int crownsToWin) throws IOException {
         players = new ArrayList<>();
         
@@ -18,5 +26,78 @@ public class Game {
         this.questionSet = new QuestionSet(setFile.getPath());
         
         this.crownsToWin = crownsToWin;
+        
+        crownHolders = new ArrayList<>();
+        crownTimes = new ArrayList<>();
+        
+        for (int i = 0; i < questionSet.getCategories().size(); i++) {
+            crownHolders.add(NO_ONE);
+            crownTimes.add(Long.MAX_VALUE);
+        }
+    }
+    
+    public boolean someoneWon() {
+        int crownsHeld;
+        for (Player player :players) {
+            crownsHeld = 0;
+            for (Player crownHolder : crownHolders) {
+                if (player.getName().equals(crownHolder.getName())) {
+                    crownsHeld++;
+                }
+            }
+            
+            if (crownsHeld >= crownsToWin) {
+                winner = player;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean updateCrowns(String category, long time, Player player) {
+        int catIndex = questionSet.getCategories().indexOf(category);
+        if (catIndex == -1) {
+            throw new IllegalArgumentException("Unknown category: " + category);
+        }
+        
+        if (time < crownTimes.get(catIndex)) {
+            crownTimes.set(catIndex, time);
+            crownHolders.set(catIndex, player);
+        }
+        
+        return someoneWon();
+    }
+    
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+    
+    public Player getPlayer(int index) {
+        return players.get(index);
+    }
+    
+    public Question getNewQuestion() {
+        currentQuestion = questionSet.getRandomQuestion();
+        return currentQuestion;
+    }
+    
+    public Question getCurrentQuestion() {
+        return currentQuestion;
+    }
+    
+    public int getCrownsToWin() {
+        return crownsToWin;
+    }
+    
+    public ArrayList<Player> getCrownHolders() {
+        return crownHolders;
+    }
+    
+    public ArrayList<Long> getCrownTimes() {
+        return crownTimes;
+    }
+    
+    public Player getWinner() {
+        return winner;
     }
 }
